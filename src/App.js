693dbc2cdb8f4496334360062a8444802db12064
feychou <federica.recanatini@gmail.com>
2021-07-08 15:38:18 +0200

    use hook for result

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import {
   MEDIUM_SPA,
   SIMPLE_SPA
 } from './outcomes';
+import useResult from './useResult';
 import QuizSection from './components/QuizSection';
 import Header from './components/Header';
 import Modal from './components/Modal';
@@ -24,66 +25,28 @@ import './App.css';
 const quizSections = [ LAYOUT, FRONTEND, BACKEND ];
 
 function App() {
-  const [checked, setChecked] = useState([]);
   const [openModal, setOpenModal] = useState(false);
-  const [outcome, setOutcome] = useState(outcomes[SIMPLE_SPA]);
-
-  const onChangeChecked = (question, cat) => {
-    const newChecked = checked.find(item => item.id === question.id) ?
-      checked.filter(item => item.id !== question.id) :
-      [...checked, {...question, cat}]
-
-    setChecked(newChecked)
-  }
+  const [
+    checked,
+    setChecked,
+    outcome,
+    setOutcome
+  ] = useResult();
 
   const toggleModal = () => {
     setOpenModal(!openModal)
   }
 
-  const chooseOutcome = (score) => {
-    const scoreBackend = score[BACKEND];
-    const scoreFrontend = score[FRONTEND];
-
-    if (scoreBackend > 60 && checked.find(item => item.id === AUTH) && checked.find(item => item.id === PRIVATE_ROUTES)) {
-      return outcomes[FULL_STACK_AUTH]
-    }
-
-    if (scoreBackend > 60) {
-      return outcomes[FULL_STACK]
-    }
-
-    if (scoreFrontend > 70) {
-      return outcomes[ADVANCED_SPA]
-    }
-
-    if (scoreFrontend <= 70 && score[FRONTEND] > 40) {
-      return outcomes[MEDIUM_SPA]
-    }
-
-    return outcomes[SIMPLE_SPA]
-  }
-
   const getResult = () => {
-    const score = checked.reduce((acc, question) => {
-      const prop = question.cat;
-
-      return {
-        ...acc,
-        [prop]: acc[prop] + question.points
-      }
-
-
-    }, {[LAYOUT]: 0, [FRONTEND]: 0, [BACKEND]: 0});
-
-    setOutcome(chooseOutcome(score))
-    toggleModal()
+    setOutcome();
+    toggleModal();
   }
 
   return (
     <div className="App">
       <div className={`Backdrop ${openModal && 'openModal'}`}>
         <Header />
-        {quizSections.map(type => <QuizSection type={type} onChange={onChangeChecked} />)}
+        {quizSections.map(type => <QuizSection type={type} onChange={setChecked} />)}
         <button onClick={getResult} onKeyPress={toggleModal}>My result</button>
       </div>
       {openModal && <Modal toggle={toggleModal} content={outcome} />}
---
+++ src/useResult.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import {
+  FRONTEND,
+  LAYOUT,
+  BACKEND,
+  AUTH,
+  PRIVATE_ROUTES
+} from './quizData';
+import {
+  outcomes,
+  FULL_STACK_AUTH,
+  FULL_STACK,
+  ADVANCED_SPA,
+  MEDIUM_SPA,
+  SIMPLE_SPA
+} from './outcomes';
+
+const initialOutcome = outcomes[SIMPLE_SPA]
+const initialScore = {[LAYOUT]: 0, [FRONTEND]: 0, [BACKEND]: 0};
+
+const chooseOutcome = (score, doTheyKnowAuth) => {
+  const scoreBackend = score[BACKEND];
+  const scoreFrontend = score[FRONTEND];
+
+  if (scoreBackend > 60 && doTheyKnowAuth) {
+    return outcomes[FULL_STACK_AUTH]
+  }
+
+  if (scoreBackend > 60) {
+    return outcomes[FULL_STACK]
+  }
+
+  if (scoreFrontend > 70) {
+    return outcomes[ADVANCED_SPA]
+  }
+
+  if (scoreFrontend <= 70 && score[FRONTEND] > 40) {
+    return outcomes[MEDIUM_SPA]
+  }
+
+  return outcomes[SIMPLE_SPA]
+}
+
+function useResult() {
+  const [checked, setChecked] = useState([])
+  const [outcome, setOutcome] = useState(initialOutcome);
+
+  const onChangeChecked = (question, cat) => {
+    const newChecked = checked.find(item => item.id === question.id) ?
+      checked.filter(item => item.id !== question.id) :
+      [...checked, {...question, cat}]
+
+    setChecked(newChecked)
+  }
+
+  const doTheyKnowAuth = () => {
+    return (
+      checked.find(item => item.id === AUTH) &&
+      checked.find(item => item.id === PRIVATE_ROUTES)
+    )
+  }
+
+  const getResult = () => {
+    const score = checked.reduce((acc, question) => {
+      const prop = question.cat;
+
+      return {
+        ...acc,
+        [prop]: acc[prop] + question.points
+      }
+
+
+    }, initialScore);
+
+    setOutcome(chooseOutcome(score, doTheyKnowAuth()))
+  }
+
+  return [
+    checked,
+    onChangeChecked,
+    outcome,
+    getResult
+  ];
+}
+
+export default useResult;
